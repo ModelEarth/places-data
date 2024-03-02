414bfcfc8e98ae1df1a33e04ce6172f24355e1ae
Lokakaaa <69859833+Lokakaaa@users.noreply.github.com>
2024-03-02 15:03:26 -0600

    csv output

--- alltheplaces_to_csv.py
+++ alltheplaces_to_csv.py
@@ -2,6 +2,7 @@ import os
 from os.path import isfile, join
 import json
 import csv
+import re
 
 def main():
     alltheplaces_dir = 'output'
@@ -23,45 +24,57 @@ def main():
 
             for feature in data['features']:
                 prop = feature['properties']
-                if 'addr:country' not in prop:
-                    try:
-                        zipcode = str(prop.pop('addr:postcode')).replace(" ", "")
-                    except KeyError as e:
-                        print(f"KeyError for 'addr:postcode' in file {file}: {e}")
-                        continue  # Skip if 'addr:postcode' is missing
-                    
-                    # Determine the directory and filename based on the zipcode
-                    if zipcode[:3].isdigit() and len(zipcode) >= 5:
-                        csv_dir = f"location/postalcode/US/{zipcode[:3]}/"
-                        csv_filename = f"{zipcode}.csv"
-                    else:
-                        csv_dir = "location/postalcode/international/"
-                        csv_filename = "international.csv"
-                    
-                    csv_path = join(csv_dir, csv_filename)
-                    print(csv_dir)
+                country = prop.get('addr:country', 'unknown')
 
-                    os.makedirs(csv_dir, exist_ok=True)
+                if country != 'unknown':
+                    country_folder = os.path.join('location', 'postalcode', country.upper())
+                else:
+                    country_folder = 'location/postalcode/unknown'
+
+                zipcode = str(prop.pop('addr:postcode', "unknown"))                    
+                
+                # Replace special characters in zipcode
+                zipcode = re.sub(r'\s+', '', zipcode)  # Remove whitespace characters
+                zipcode = re.sub(r'<br>', '', zipcode)  # Remove '<br>'
+                zipcode = re.sub(r'\r\n', '', zipcode)  # Remove '\r\n'
+                zipcode = re.sub(r'[^\w\s]', '', zipcode)  # Remove non-alphanumeric characters
+                
+                # Remove any empty or invalid zipcodes
+                if zipcode == "NULL":
+                    zipcode = "unknown"
+                
+                # Determine the directory and filename based on the zipcode
+                if country == 'US':
+                    csv_dir = os.path.join(country_folder, zipcode[:3])
+                else:
+                    csv_dir = country_folder
+
+                csv_filename = f"{zipcode}.csv"
+                csv_path = join(csv_dir, csv_filename)
+                print(csv_dir)
+
+                os.makedirs(csv_dir, exist_ok=True)
                     
-                    csv_data = {
-                        "Ref": prop.get("ref", ""),
-                        "Spider": prop.get("@spider", ""),
-                        "Name": prop.get("name", ""),
-                        "Address": prop.get("addr:full", ""),
-                        "City": prop.get("addr:city", ""),
-                        "State": prop.get("addr:state", ""),
-                        "Phone": prop.get("phone", ""),
-                        "Website": prop.get("website", ""),
-                        "Hours": prop.get("opening_hours", ""),
-                        "Brand": prop.get("brand", ""),
-                        "WikiData": prop.get("brand:wikidata", "")
-                    }
+                csv_data = {
+                    "Ref": prop.get("ref", ""),
+                    "Spider": prop.get("@spider", ""),
+                    "Name": prop.get("name", ""),
+                    "Address": prop.get("addr:full", ""),
+                    "City": prop.get("addr:city", ""),
+                    "State": prop.get("addr:state", ""),
+                    "Country": country,  # Add country column
+                    "Phone": prop.get("phone", ""),
+                    "Website": prop.get("website", ""),
+                    "Hours": prop.get("opening_hours", ""),
+                    "Brand": prop.get("brand", ""),
+                    "WikiData": prop.get("brand:wikidata", "")
+                }
                     
-                    with open(csv_path, mode='a', newline='', encoding='utf-8') as csv_file:
-                        writer = csv.DictWriter(csv_file, fieldnames=csv_data.keys())
-                        if not os.path.isfile(csv_path) or os.stat(csv_path).st_size == 0:
-                            writer.writeheader()
-                        writer.writerow(csv_data)
+                with open(csv_path, mode='a', newline='', encoding='utf-8') as csv_file:
+                    writer = csv.DictWriter(csv_file, fieldnames=csv_data.keys())
+                    if not os.path.isfile(csv_path) or os.stat(csv_path).st_size == 0:
+                        writer.writeheader()
+                    writer.writerow(csv_data)
 
 if __name__ == "__main__":
     main()
